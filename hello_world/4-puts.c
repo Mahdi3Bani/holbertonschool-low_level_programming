8dae3b0c903590f91ac8cc8ce4febbd096ebfa9e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-06 04:27:06 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-/*using puts instead of printf */
-int main(void )
+/**
+ * main - Entry point
+ *
+ * using puts instead of printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
