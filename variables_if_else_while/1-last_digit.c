0b15362ef5a3b8e8951d8b516ba66e1c4b209254
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-07 03:51:07 -0700

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/*
- *main - detecting the last digigt and check its value
- *
- *alwayd return 0
- **/
+/**
+* main - detecting the last digigt and check its value
+*
+* Return: 0
+*/
 int main(void)
 {
 	int n;
@@ -17,15 +16,15 @@ int main(void)
 	a = n % 10;
 	if (a > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",n ,a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
-	else if (a == 0 )
+	else if (a == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n",n ,a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 	else if (a < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n ,a);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
