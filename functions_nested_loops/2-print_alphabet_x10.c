24e829e210197ae9520a7cdacd771e6ccb961651
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-09 03:03:55 -0700

    *10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 time
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i < 11; i++)
+		print_alphabet();
+
+}
