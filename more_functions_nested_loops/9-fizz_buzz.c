ed6200ca1060b6c2e1e0d8f7a7e5b7fbbcad4ca9
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-11 15:08:40 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: 0 succes
+ */
+int main(void)
+{
+int i;
+
+	for (i = 1; i < 100 ; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz");
+	_putchar('\n');
+
+
+
+}
