ca1a5bfb2af8575e36a40eaeea3bab27c50fc9ab
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-11 15:16:11 -0700

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int i;
 	for (i = 1; i < 100 ; i++)
 	{
 		if (i % 15 == 0)
-                        printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
