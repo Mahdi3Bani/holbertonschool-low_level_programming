0118f0a522af5042b13e706a50d881daaa93cd01
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-11 05:34:51 -0700

    times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	_putchar(48);
+	for (i = 0; i < 11; i++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(48);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(48);
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(48 + j);
+
+		}
+
+
+	}
+
+
+
+
+
+
+}
