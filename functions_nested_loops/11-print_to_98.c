0e2136e9828d1c2b2aede34df9a77f7c419729c5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-11 06:08:15 -0700

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - printing numbers until 98
+ * @n: global var
+ * Return: s
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(' ');
+			_putchar(',');
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(' ');
+			_putchar(',');
+
+		}
+	}
+	printf(" 98\n");
+}
