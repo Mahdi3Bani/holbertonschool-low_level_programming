e68b38bb7bf6794352ea4bf1fbba0cb0b2619e8e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-14 00:41:24 -0700

    Half the lies they tell about me arent true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string, starting with the first
  * @str: the pointer of str
  */
 void puts2(char *str)
 {
 
-	int i;
+	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
+		j = j + 1;
+
+	for (i = 0; i < j; i += 2)
 		_putchar(str[i]);
-		i++;
-	}
 	_putchar('\n');
 
 
