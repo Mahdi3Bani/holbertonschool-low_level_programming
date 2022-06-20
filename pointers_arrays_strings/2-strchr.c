5f327b1a9da65eed3e0e552f6ec945b7f77dfc7a
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-20 07:18:30 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,9 +3,7 @@
  * *_strchr - locates a character in a string
  * @s: string
  * @c: char
- *
  * Return: a pointer to the first occurrence of the character
- * c in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
