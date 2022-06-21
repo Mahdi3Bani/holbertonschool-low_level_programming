45f4dd434121e0e59c5f32f23bd1211d9c0addf5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-21 10:14:50 +0100

    prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+int prime(int n, int i);
+/**
+ * prime - find the prime number
+ * @n: global var
+ * @i: global var
+ * Return: 0 or -1
+ */
+int prime(int n, int i)
+{
+if (i > (n / 2))
+return (0);
+if (n / i == 0)
+return (1);
+else
+sqr(n, i + 1);
+
+
+
+
+}
+/**
+ * is_prime_number - returns 1 if the integer is a prime number,return 0
+ * @n: the input int
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+int i = 1;
+
+if (n == 1 || n == 0)
+return (n);
+else
+return (prime(n, i + 1));
+
+
+
+
+}
