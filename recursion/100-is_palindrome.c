66b162d8948302d55353aa2d0a5ec94ef83486fb
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-21 10:51:12 +0100

    palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+int pal(char *s, int i, int n);
+/**
+ * pal - palindrome
+ * @s: pointer
+ * @i: global var
+ * @n: size of s
+ * Return: 1 or 0
+ */
+int pal(char *s, int i, int n)
+{
+if (i == n)
+return (1);
+if (s[i] == s[n - 1])
+return (pal(*s, i + 1, n - 1));
+else
+return (0);
+
+
+
+}
+
+
+
+
+
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: pointer of s
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+int i;
+int n;
+
+i = 0;
+n = 0;
+for (i = 0; str[i] != '\0'; ++i)
+{
+_n = n + 1;
+}
+i = 0;
+return (pal(*s, i, n));
+
+
+
+}
