3c25e2f3678c93b6806a3c7adaabef1488b116fd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-21 09:58:30 +0100

    sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,21 @@
 #include "main.h"
+int sqr(int n, int i);
+/**
+ * sqr - square of number
+ * @n: global var
+ * @i: global var
+ * Return: square of number
+ */
+int sqr(int n, int i)
+{
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	else
+		return (sqr(n, i + 1));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: the number
@@ -6,12 +23,12 @@
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
+	int i;
 
-else
-return ((n - (n - 1) * _sqrt_recursion(n - 1)));
+	i = 0;
+	if (n < 0)
+		return (-1);
 
-
-
+	else
+		return (sqr(n, i + 1));
 }
