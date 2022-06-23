a3cfbf3261ca40569a6bceb7d933e2c6ef2300c1
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-23 15:06:05 +0100

    WHATS MY NAME?

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
-#include
+#include "stdio.h"
 /**
  * main - print its name
- * 
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
