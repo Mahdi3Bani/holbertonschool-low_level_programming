d7864d18a5ec20fcce45ec032dfd1d48a2d5fcfb
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-24 17:09:04 +0100

    multiple

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,8 +8,13 @@
 int main(int argc, char *argv[])
 {
 
-printf("%d\n", argv[1] * agrv[2]);
 
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[1]));
 
 	return (0);
 }
