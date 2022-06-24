cdeb0dc26b790f20c6a81add4432318922a8c764
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-24 16:51:31 +0100

    multiple

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+/**
+ * main - multiple
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+printf("%d\n", argv[1] * agrv[2]);
+
+
+	return (0);
+}
