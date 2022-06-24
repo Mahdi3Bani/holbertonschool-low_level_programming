7fded99218b349f82341e6a7a9bc7c65c110bcb8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-24 17:12:59 +0100

    multiple

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiple
  * @argc: number of arguments
@@ -9,7 +10,7 @@ int main(int argc, char *argv[])
 {
 int res;
 
-if (argc < 3 || argc > 3)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
