5c03c86290b7185fbe1e0aaf1886ca5868da64a9
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-28 15:10:49 +0100

    checking :0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: memory indeed
+ */
+void *malloc_checked(unsigned int b)
+{
+void *s;
+
+s = malloc(b);
+if (s == NULL)
+{
+exit(98);
+}
+return(s);
+}
