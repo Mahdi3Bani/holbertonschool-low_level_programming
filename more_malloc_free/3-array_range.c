c773875ade08a8d26b8384fb43a06a1fc94b57ae
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-29 01:24:44 +0100

    rage table

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * array_range - creates an array of integers.
+ * @min: min
+ * @max: max
+ * Return: s
+ */
+int *array_range(int min, int max)
+{
+int *s, size;
+
+if (min > max)
+return (NULL);
+
+size = max - min;
+s = malloc(sizeof(int) * size);
+if (s == NULL)
+return (NULL);
+i = 0;
+while (min <= max)
+{
+s[i] = min++;
+i++;
+}
+return (s);
+}
