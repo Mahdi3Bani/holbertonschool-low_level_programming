95a8275c148a7444ca09231a0cf4bd07f3128207
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-06-30 16:05:41 +0100

    the dogger file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H_
 #define DOG_H_
+#include <stdlib.h>
 /**
  * struct dog - new structure type
  * @name: dog name
@@ -12,4 +13,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
