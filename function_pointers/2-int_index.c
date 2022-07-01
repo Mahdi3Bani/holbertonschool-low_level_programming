37dceb802197a698bb957bd05d12bfd166a534fd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-01 22:15:21 +0100

    compare me senpai

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns the index of the first element for which
+ * the cmp function does not return 0
+ * @array: an array : D
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: -1 for fail, 0 for succes
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 0)
+return (i);
+}
+
+
+
+
+}
