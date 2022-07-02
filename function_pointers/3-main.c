8585b7720258a34debe97e3a61da4130bb62b524
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-02 22:18:04 +0100

    the main file

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-
 /**
  * main - program that perfroms simple operations
  * @argc: number of arguments
@@ -7,3 +6,25 @@
  *
  * Return: Always 0 (Success)
  */
+int main(int argc, char *argv[])
+{
+int r;
+int (*f)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(1);
+}
+
+f = get_op_func(argv[2]);
+if (!f)
+{
+printf("Error\n");
+exit(1);
+}
+r = func(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", r);
+return (0);
+
+}
