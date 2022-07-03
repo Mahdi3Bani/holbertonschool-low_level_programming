ef93dc75493c6c905bd89970bb799165b5eea736
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-03 11:17:43 +0100

    opcode

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int a;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+a = atoi(argv[1]);
+if (a < 0)
+{
+printf("Error\n");
+exit(2);
+}
+}
