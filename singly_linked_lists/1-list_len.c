3c191e43c84164d7d86f68e69cded4963fbe53c8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-14 15:57:03 +0100

    length?

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * list_len - number of elements in a linked list
+ * @h: pointer to the head
+ * Return: length
+ **/
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	else
+		return (1 + list_len(h->next));
+}
