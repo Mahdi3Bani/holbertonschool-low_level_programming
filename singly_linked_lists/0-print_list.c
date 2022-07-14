355e7a1191276098cb18356f068d1b6320f4672d
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-14 15:47:47 +0100

    recruissive agin

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,19 +6,12 @@
  */
 size_t print_list(const list_t *h)
 {
-if (h == NULL)
+	if (h == NULL)
 		return (0);
-if (h->str == NULL)
-	printf("[0] (nil)\n");
-else
-	printf("[%d] %s\n", h->len, h->str);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
 
-return (1 + print_list(h->next));
-
-
-
-
-
-
-
+	return (1 + print_list(h->next));
 }
