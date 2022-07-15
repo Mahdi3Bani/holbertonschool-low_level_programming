cc017baef0c259846845f5f03b8e65ec3a277902
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-15 22:18:35 +0100

    count me

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h);
+{
+	if (h == NULL)
+		return (0);
+	if (h->n == 0)
+		printf("0");
+	else
+		printf("%d\n", h->str);
+
+	return (1 + print_list(h->next));
+}
