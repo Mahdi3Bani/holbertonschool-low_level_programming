7bf9fb5643451791c1b166e0d1b74ac2fee5e0c4
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-15 22:45:36 +0100

    free

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: The list to free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *A;
+
+	while (head)
+	{
+		A = head->next;
+		free(head->n);
+		free(head);
+		head = A;
+	}
+}
