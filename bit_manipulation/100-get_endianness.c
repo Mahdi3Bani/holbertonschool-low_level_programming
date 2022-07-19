e7773a5e6d000e78cfb9ef97b96e1fe8f01649d7
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-19 23:11:25 +0100

    endianness

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,31 +1,16 @@
 #include "main.h"
 
 /**
- * show_mem_rep - show bytes in memory, from location start to start+n
- *
- * @start: start
- * @n: n
- */
-
-void show_mem_rep(char *start, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-		putchar(start[i] + '0');
-	putchar('\n');
-}
-
-/**
  * get_endianness - checks the endianness.
  *Return: 0
  */
 int get_endianness(void)
 {
 
-	int i = 0x01234567;
+	int i = 1;
 
-	show_mem_rep((char *)&i, sizeof(i));
-	getchar();
-	return (0);
+	if (*((char *)&i) == 1)
+		return (1);
+	else
+		return (0);
 }
