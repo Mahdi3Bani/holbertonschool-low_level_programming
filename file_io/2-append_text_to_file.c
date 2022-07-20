1378d482079ada42525122ed8948d9b89e5ca285
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-20 23:29:23 +0100

    append

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -7,26 +7,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, s, t;
+	int fd, s;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+
+	s = write(fd, text_content, strlen(text_content));
+
+	if (s == -1)
 	{
-		while (text_content[s])
-			s++;
-		t = write(fd, text_content, s);
-		if (t != s)
-			return (-1);
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
-
 	return (1);
 }
