12fa33263800da8602246a6b35d6e7ef69106acd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-20 01:47:54 +0100

    the header file

--- file_io/main.h
+++ file_io/main.h
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <errno.h>
-#include<string.h> 
+#include<string.h>
 #include<unistd.h>
 
 
