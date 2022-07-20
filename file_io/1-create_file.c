5fbf417a0a71c5a35f80f7c5f5296adc137253d3
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-20 13:53:27 +0100

    create me

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_file - create a file
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 or -1
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int c;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	c = write(fd, text_content, strlen(text_content));
+	if (c == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
