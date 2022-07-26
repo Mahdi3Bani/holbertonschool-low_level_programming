0eb43c643448c1e860b79fffb28956108ea8acee
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-26 15:18:01 +0100

    add me

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(node);
 		return (NULL);
 	}
-	node->prev = node->next;
+	
+	node->prev = NULL;
 	node->next = *head;
+	if (*head)
+		(*head)->prev = node;
 	*head = node;
 
 	return (node);
