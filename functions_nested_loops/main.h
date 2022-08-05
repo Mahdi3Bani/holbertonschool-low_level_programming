8c3a07f834cc24137388edf243d36b6667cfd9da
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-05 23:06:31 +0100

    the header file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,19 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
-<<<<<<< HEAD
-=======
 void times_table(void);
->>>>>>> ef22afe81598a58b339a37cbdb750bd7835c0240
-void print_to_98(int n);
+void print_to_98(int);
+void print_times_table(int n);
 #endif
