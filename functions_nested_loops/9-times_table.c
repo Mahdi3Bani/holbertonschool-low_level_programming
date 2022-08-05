844c1ade0f2eff15b3aa5d35f5da23bcaa3d5a68
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-05 23:06:10 +0100

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,30 +8,25 @@ void times_table(void)
 	int i;
 	int j;
 
-	_putchar(48);
+	putchar(48);
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(48);
+		putchar(',');
+		putchar(' ');
+		putchar(' ');
+		putchar(48);
 	}
-	_putchar('\n');
+	putchar('\n');
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(48);
+		putchar(48);
 		for (j 	= 1; j < 10; j++)
 		{
-			printf(",  %d", i * 2);
+			printf(",  %d", i * 3);
 
 		}
-		_putchar('\n');
+		putchar('\n');
 
 	}
-
-
-
-
-
 
 }
