2c383e2bd9a264ff50c72c2e4a3efbcc3e2a340d
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-21 15:01:48 +0100

    print

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - ints a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	int test = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i])
+			{
+				if (test > 0)
+					printf(",");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				test++;
+				ht->array[i] = ht->array[i]->next;
+			}
+		}
+	}
+	printf("}\n");
+}
