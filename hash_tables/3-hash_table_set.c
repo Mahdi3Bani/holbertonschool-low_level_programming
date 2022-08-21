4712e7fc366dceaa3eb9adb3fe464cbe9fffed4f
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-21 14:52:05 +0100

    set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || ht == NULL)
 		return (0);
 
-	index = key_index(key, ht->size);
+	index = key_index((unsigned char *) key, ht->size);
 
 	tmp = ht->array[index];
 	while (tmp)
